4fb5a40f1e5bf706a4ff36e6a17d08d103f22214
Harshal <harshal@dbspl.co.in>
2024-09-14 17:15:18 +0530

    leetcode" "

---
+++ Exercise/Climbing stairs.py
@@ -0,0 +1,13 @@
+def climbStairs( n: int) -> int:
+  if n == 1:
+    return 1
+  if n == 2:
+    return 2
+
+  p1 = 2
+  p2 = 1
+  for i in range(3, n + 1):
+    current = p1 + p2
+    p2 = p1
+    p1 = current
+  return p1
